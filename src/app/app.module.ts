a6ffe87c59c1e9a8533633318763585d3ab9c461
Shiva <shiva.sairam97@gmail.com>
2019-01-27 20:40:21 +0530

    initial commit with toolbar, sidenav and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// imports for material design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { EmiComponent } from './emi/emi.component';
+import { ProfileComponent } from './profile/profile.component';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+
+
+
+// list of urls for the routing module
+const appRoutes: Routes = [
+  { path: 'transaction', component: TransactionsComponent },
+  { path: 'emi', component: EmiComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'auth', component: AuthComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  { path: '**', component:  HomeComponent}
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TransactionsComponent,
+    EmiComponent,
+    ProfileComponent,
+    HomeComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule .forRoot(appRoutes) ,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatMenuModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
